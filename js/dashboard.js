51673d40f61163467c7eea95b81fb4e8a155a72d
Caroline Yu <carolineyu@umass.edu>
2017-06-03 10:56:23 -0400

    new stuff

---
+++ js/dashboard.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('[data-toggle="offcanvas"]').click(function(){
+        $("#navigation").toggleClass("hidden-xs");
+    });
+});/**
+ * Created by Bella on 2017/6/1.
+ */
