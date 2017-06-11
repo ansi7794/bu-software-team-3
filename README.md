666925f5f27c1a4347e68b4e26402762600656d1
Caroline Yu <carolineyu@umass.edu>
2017-06-11 11:26:20 -0400

    new things

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # bu-software-team-3
 BU Summer Term CS 473/673 
 
-Caroline test
+Notes: Right now the html file goes to login.js when the form submits. Can make register redirect to html and login page submit the form. If I do that, the loginFunction() can be deleted. OR I can make an onclick function but a lot of parameters will be passed in.
+
+
+Right now it's configured like the first one. There is no onclick, only a submit, on the login button in login.html. 
--- js/login.js
+++ js/login.js
@@ -1,36 +1,39 @@
 // Attach a submit handler to the form
 $("#login_form").submit(function( event ) {
+    var data;
+    var myJSON = {"user_name" : $('#username').val(), "user_password" :  $('#password').val()}
 
+    data = "txn=1"+"&user_name="+ $('#username').val() +"&user_password=" + $('#password').val();
     // Stop form from submitting normally
     event.preventDefault();
 
     // Get some values from elements on the page:
-    var $form = $( this );
+    var $form = $( this );  
 
-    //changed from login to username
     // We want to customize what we post, therefore we format our data
-    var data = "login="+ $('#username').val() +" & password=" + $('#password').val();
-
+    
     // For debugging purposes... see your console:
     // Prints out for example: login=myLoginName&passwordHash=a011a78a0c8d9e4f0038a5032d7668ab
+    console.log(myJSON);
     console.log(data);
-    alert(data);
     // The actual from POST method
     $.ajax({
         type: $form.attr('method'),
-        url:  $form.attr('action'),
-        data: data,
+        url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com:8080/idea/idea.jsp?",//$form.attr('action'),
+        dataType: "jsonp",
+        data: myJSON,
         success: function (data) {
             console.log("Hey, we got reply form java side, with following data: ");
             console.log(data);
-
             // redirecting example..
             if(data === "SUCCESS") {
-
-               window.location.replace("");
-
+              window.location.replace("action");
             }
+        },
 
-        }
+        error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
     });
 });
