c2e7f1f1489fe067cc93c01ded6d0c4ec0606da1
ansi7794 <anushree@bu.edu>
2017-06-15 18:41:09 -0400

    Changes_using_json

--- js/login.js
+++ js/login.js
@@ -1,19 +1,18 @@
 // Attach a submit handler to the form
 $("#login_form").submit(function( event ) {
     var data = "txn=1"+"&user_name="+ $('#user_name').val() +"&user_password=" + $('#user_password').val();
-    var tocheck = "\"user_name\":\"" + $('#user_name').val() + "\""
+    var user = $('#user_name').val();
     // Stop form from submitting normally
     event.preventDefault();
 
     // Get some values from elements on the page:
     var $form = $( this );  
-
     // We want to customize what we post, therefore we format our data
     
     // For debugging purposes... see your console:
     // Prints out for example: login=myLoginName&passwordHash=a011a78a0c8d9e4f0038a5032d7668ab
     console.log(data);
-    console.log(tocheck)
+    console.log(user);
 
     // The actual form GET method
     $.ajax({
@@ -24,10 +23,9 @@ $("#login_form").submit(function( event ) {
             console.log("Hey, we got reply form java side, with following data: ");
             console.log(info);
             myJSON = JSON.parse(info);
-            console.log(myJSON);
             console.log("testing success");
             // redirecting example..
-            if(info.indexOf(tocheck)>-1) {
+            if(myJSON.user_name == user) {
                 window.location.replace("projectSample.html");
             }
             else {
