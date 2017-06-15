8f0867e60f9f36be69799205242687e9899a74fe
ansi7794 <anushree@bu.edu>
2017-06-15 18:08:51 -0400

    Changes_by_anu

--- js/register.js
+++ js/register.js
@@ -1,40 +1,47 @@
 // Attach a submit handler to the form
 $("#register_form").submit(function( event ) {
-	var data;
-    var myJSON = {"user_name" : $('#username').val(), "user_password" :  $('#password_1').val(), "user_fname":  $('#first_name').val(), "user_lname": $('#last_name').val(), "user_email": $('#email_address').val()}
+    if ($('#user_password1').val() == $('#user_password2').val()) { 
+        var data = "txn=1"+"&user_name="+ $('#user_name').val() +"&user_password=" + $('#user_password1').val();
+        var tocheck = "{\"user_name\":\"" + $('#user_name').val() + "\"}"
+        // Stop form from submitting normally
+        event.preventDefault();
 
-    data = "txn=2"+"&first_name"+ $('#first_name').val()+"&last_name"+ $('#last_name').val()+"&user_name="+ $('#email_address').val() +"&user_password=" + $('#password_1').val();
-    // Stop form from submitting normally
-    event.preventDefault();
+        // Get some values from elements on the page:
+        var $form = $( this );  
 
-    // Get some values from elements on the page:
-    var $form = $( this );  
+        // We want to customize what we post, therefore we format our data
+        
+        // For debugging purposes... see your console:
+        // Prints out for example: login=myLoginName&passwordHash=a011a78a0c8d9e4f0038a5032d7668ab
+        console.log(data);
+        console.log(tocheck)
 
-    // We want to customize what we post, therefore we format our data
-    
-    // For debugging purposes... see your console:
-    // Prints out for example: login=myLoginName&passwordHash=a011a78a0c8d9e4f0038a5032d7668ab
-    console.log(data);
-    console.log(myJSON);
-
-    // The actual from POST method
-    $.ajax({
-        type: $form.attr('method'),
-        url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com:8080/idea/idea.jsp?",//$form.attr('action'),
-        dataType: "jsonp",
-        data: myJSON,
-        success: function (data) {
-            console.log("Hey, we got reply form java side, with following data: ");
-            console.log(myJSON);
-            // redirecting example..
-            if(data === "SUCCESS") {
-              window.location.replace("action");
-            }
-	    },
-
-        error: function (data) {
-                console.log('An error occurred.');
-                console.log(data);
+        // The actual form GET method
+        $.ajax({
+            type: $form.attr('method'),
+            url:  $form.attr('action'),
+            data: data,
+            success: function (info) {
+                console.log("Hey, we got reply form java side, with following data: ");
+                console.log(info);
+                console.log("testing success");
+                // redirecting example..
+                if(info.indexOf(tocheck)>-1) {
+                    window.location.replace("projectSample.html");
+                }
+                else {
+                    alert("Invalid Username or Password");
+                }
             },
-    });
+
+            error: function (data) {
+                    console.log('An error occurred.');
+                    console.log(data);            },
+        });
+    }
+    else {
+        alert("Your password and confirmation password do not match.");
+        $('#user_password2').focus();
+        return false;
+    }
 });
