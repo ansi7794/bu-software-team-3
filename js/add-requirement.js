a4a230a2969a0bb55540d9a5c7f977bdad253ba3
ansi7794 <anushree@bu.edu>
2017-07-06 19:49:10 -0400

    add_new_req

--- js/add-requirement.js
+++ js/add-requirement.js
@@ -1,19 +1,17 @@
-$("#add-requirement").click(function( event ) {
+$("#add-requirement2").click(function( event ) {
   var SearchString = String(document.URL);
   var arr = SearchString.split('?');
   console.log(arr[1]);
-  var res = String(arr[1]).replace(/%22/g, "\"");
-  console.log(res);
-  requirementJSON=JSON.parse(res);
-  requirement_name = $('#requirement_title').val();
-  requirement_status = $("#requirement_status").val();
+  console.log("new2");
+  requirement_name = $('#requirements_title').val();
+  requirement_status = $("#requirements_status").val();
   requirement_description = $('#requirements_description').val();
   user_name = $('#requirements_assignee').val();
-  project_id = $('#project_id').val();
-  data = "txn=11&requirement_name="+requirement_name+"&requirement_status="+requirement_status+"&requirement_description="+requirement_description+"&user_name="+user_name+"&project_id="+project_id;
+  project_id = arr[1];
+  var data = "txn=11&requirement_name="+requirement_name+"&requirement_status="+requirement_status+"&requirement_description="+requirement_description+"&user_name="+user_name+"&project_id="+project_id;
   console.log(data);
   $.ajax({
-    type: "POST",
+    type: "GET",
     url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
     data: data,
     success: function (info) {
