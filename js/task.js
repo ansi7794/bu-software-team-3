ee5be0bc3f97dad9d10a52898f80dcd716190f6b
ansi7794 <anushree@bu.edu>
2017-07-06 18:59:39 -0400

    add_task_complete

--- js/task.js
+++ js/task.js
@@ -4,22 +4,21 @@ $("#add-task1").click(function (event) {
    console.log("add task = " + arr[1]);
    // var res = String(arr[1]).replace(/%22/g, "\"");
    // console.log("res = " + res);
-   taskJSON = JSON.stringify(arr[1]);
-   console.log(taskJSON);
+   var requirement_id = arr[1];
    task_name = $('#add_task_name').val();
-   console.log(task_name);
    task_status = $('#add_task_status').val();
    task_description = $('#add_task_description').val();
    user_name = $('#add_task_assignee').val();
+
    var data = "txn=13" +
-           "&requirement_id=" + taskJSON.requirement_id +
            "&task_name=" + task_name +
            "&task_status=" + task_status +
            "&task_description=" + task_description +
-           "&user_name=" + user_name;
+           "&user_name=" + user_name +
+           "&requirement_id=" + requirement_id;
    console.log(data);
    $.ajax({
-      type:"POST",
+      type:"GET",
       url:"http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
       data: data,
       success: function(info){
