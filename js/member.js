1ab8772bf1a243c802071f9e4c9d12d6bd1e7fb3
ansi7794 <anushree@bu.edu>
2017-07-10 18:56:41 -0400

    displaying members

--- js/member.js
+++ js/member.js
@@ -1,24 +1,31 @@
-$("#add-member").click(function( event ) {
-  console.log("in");
-  var SearchString = String(document.URL);
-  console.log(SearchString);
-  var arr = SearchString.split('?');
-  console.log(arr[1]);
-  user_name = $('#user_name').val();
-  data = "txn=7&project_id="+arr[1]+"&user_name="+user_name;
-  console.log(data);
-  $.ajax({
-    type: "POST",
-    url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
-    data: data,
-    success: function (info) {
-            console.log(info);
-            window.location.reload();
+$(document).ready(function (){
+   var SearchString = String(document.URL);
+   var arr = SearchString.split('?');
+   console.log(arr[1]);
+   var data = "txn=5"+"&project_id="+ arr[1];
+   var memlist;
+   var listlen;
+    $.ajax({
+        type: "GET",
+        url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
+        data: data,
+        success: function (info) {
+          console.log("yeah");
+          console.log(info);
+          members = info.replace(/\n/gm," ").split(" ");
+          memlist = members.filter(function(v){return v.startsWith("{")});
+          listlen = memlist.length;
+          for (var i = 0; i < listlen; i++) {
+              console.log(i);
+              memJSON = JSON.parse(memlist[i]);
+              var memberinfo = "<li>"+ memJSON.user_name+"</li>";
+              $(memberinfo).appendTo("#promembers");
+          }
         },
 
-    error: function (info) {
-        console.log('An error occurred.');
-        console.log(info);
-    },
-  });
+        error: function (info) {
+            console.log('An error occurred.');
+            console.log(info);
+        },
+    });
 });
