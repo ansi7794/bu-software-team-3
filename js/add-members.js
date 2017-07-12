2f49a0aba44aa397847b32ead38e4a16c3770b0c
ansi7794 <anushree@bu.edu>
2017-07-12 09:25:30 -0400

    members_getting_added

--- js/add-members.js
+++ js/add-members.js
@@ -1,32 +1,29 @@
- $('#member-dropdown-options li').on({
-	"click":function(e){
-	    alert("trying txn 7");
-	    
-	    var SearchString = String(document.URL);
-	    var arr = SearchString.split('?');
-  		var project_id = arr[1]
-	    var user_name = $(this).attr('id');
+ $('#member-dropdown-options').on("click","li", function(){
+    alert("trying txn 7");
+    
+    var SearchString = String(document.URL);
+    var arr = SearchString.split('?');
+		var project_id = arr[1]
+    var user_name = $(this).attr('id');
 
-	    var data = "txn=7&project_id="+ project_id + "&user_name="+ user_name;
-	    console.log("PROJECT_ID: " + project_id);
-	    console.log("DATA: " + data);
-		$.ajax({
-		    type: "GET",
-		    url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
-		    data: data,
-		    success: function (info) {
-		            console.log(info);
-		            var requirement = info.replace(/\n/gm,"");
-		            var detailsJSON = JSON.parse(requirement);
-		            console.log("inside ajax call");
-		            window.location.href=window.location.href;
-		        },
+    var data = "txn=7&project_id="+ project_id + "&user_name="+ user_name;
+    console.log("PROJECT_ID: " + project_id);
+    console.log("DATA: " + data);
+	$.ajax({
+	    type: "GET",
+	    url:  "http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
+	    data: data,
+	    success: function (info) {
+	            console.log(info);
+	            var requirement = info.replace(/\n/gm,"");
+	            var detailsJSON = JSON.parse(requirement);
+	            console.log("inside ajax call");
+	            window.location.reload();
+	        },
 
-		    error: function (info) {
-		        console.log('An error occurred.');
-		        console.log(info);
-		    },
-		});
-
-	}
+	    error: function (info) {
+	        console.log('An error occurred.');
+	        console.log(info);
+	    },
+	});
 });
