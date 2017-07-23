2892123985a8be350a324d678c72b3e583ffb10f
ansi7794 <anushree@bu.edu>
2017-07-22 20:07:52 -0400

    issues table working

--- js/add_issue.js
+++ js/add_issue.js
@@ -1,12 +1,12 @@
 $("#add-issue").click(function (event) {
     // project_id = $('#issue_proj_id').val();
-    issue_name = $('#add_issue_name').val();
-    issue_severity = $('#add_issue_severity').val();
-    issue_priority = $('#add_issue_priority').val();
-    issue_type = $('#add_issue_type').val();
-    issue_description = $('#add_issue_description').val();
-    issue_found_date = $('#add_issue_founddate').val();
-    project_id = $('#add_issue_projid').val();
+    var issue_name = $('#add_issue_name').val();
+    var issue_severity = $('#add_issue_severity').val();
+    var issue_priority = $('#add_issue_priority').val();
+    var issue_type = $('#add_issue_type').val();
+    var issue_description = $('#add_issue_description').val();
+    var issue_found_date = $('#add_issue_founddate').val();
+    var project_id = $('#add_issue_projid').val();
 
     var data = "txn=27" +
         "&project_id=" + project_id +
@@ -15,14 +15,15 @@ $("#add-issue").click(function (event) {
         "&issue_priority=" + issue_priority +
         "&issue_severity=" + issue_severity +
         "&issue_date_found=" + issue_found_date +
-        "&issue_description=" + issue_found_date;
+        "&issue_description=" + issue_description;
     console.log(data);
     $.ajax({
         type:"GET",
         url:"http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
         data: data,
         success: function(info){
-            window.location.href = window.location.href;
+            console.log("function call");
+            GetIssuesByProj(project_id);
         },
         error: function(info){
             console.log('An error occurred in ADD TASK!');
@@ -30,6 +31,5 @@ $("#add-issue").click(function (event) {
         }
     });
 });
-
 
 
--- js/edit_issue.js
+++ js/edit_issue.js
@@ -1,27 +1,24 @@
 $("#edit-issue").click(function (event) {
     console.log("inside edit js");
-    var SearchString = String(document.URL);
-    var arr = SearchString.split('?');
-    console.log("arr = " + arr);
-    var idset = arr[1].split('&&&');
-    var project_id = $('#issue_proj_id').val();
-    issue_name = $('#edit_issue_name').val();
-    issue_type = $('#edit_issue_type').val();
-    issue_priority = $('#edit_issue_priority').val();
-    issue_severity = $('#edit_issue_severity').val();
-    issue_found_date = $('#edit_issue_found_date').val();
-    issue_description = $('#edit_issue_description').val();
+    var project_id = $('#edit_issue_projid').val();
+    var issue_id = $('#edit_issue_id').val();
+    var issue_name = $('#edit_issue_name').val();
+    var issue_type = $('#edit_issue_type').val();
+    var issue_priority = $('#edit_issue_priority').val();
+    var issue_severity = $('#edit_issue_severity').val();
+    var issue_found_date = $('#edit_issue_founddate').val();
+    var issue_description = $('#edit_issue_description').val();
 
     var data =
         "txn=22" +
-        "&issue_id=" + task_name +
-        "&project_id=" + task_status +
-        "&issue_name=" + task_description +
-        "&issue_type=" + task_creator +
-        "&issue_priority=" + requirement_id +
-        "&issue_severity=" + task_due +
-        "&issue_date_found=" + task_id +
-        "issue_description" + issue_description;
+        "&project_id=" + project_id +
+        "&issue_name=" + issue_name +
+        "&issue_id=" + issue_id +
+        "&issue_type=" + issue_type +
+        "&issue_priority=" + issue_priority +
+        "&issue_severity=" + issue_severity +
+        "&issue_date_found=" + issue_found_date +
+        "&issue_description=" + issue_description;
 
     console.log(data);
     $.ajax({
@@ -29,7 +26,8 @@ $("#edit-issue").click(function (event) {
         url:"http://ec2-34-201-82-113.compute-1.amazonaws.com/idea/IDEA.jsp?",
         data: data,
         success: function(info){
-            window.location.href = window.location.href;
+            console.log("function call");
+            GetIssuesByProj(project_id);
         },
         error: function(info){
             console.log('An error occurred in ADD TASK!');
